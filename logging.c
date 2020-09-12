f6f8d15b52596ce1ff63ae6d3a611f045103dcc1
alexkyou1996 <alexkyou1996@gmail.com>
2020-09-11 18:42:40 -0700

    adds function to log informations to the logging class

--- logging.c
+++ logging.c
@@ -9,10 +9,14 @@
 #include "logging.h"
 #define BUFFER_SIZE 1024
 
+//! Function to log errors
+int log_error(const char *, const char *, int, char *, ...);
+//! Function to log informational stuff
+int log_info(const char *, const char *, int, char *, ...);
 bool verbose = false;
 
 //! Function to initiate logger
-void init_log() 
+void init_log()
 {
     setlogmask(LOG_UPTO(LOG_ERR));
     openlog("test_log", LOG_NDELAY | LOG_PID, LOG_LOCAL0);
@@ -29,12 +33,12 @@ void set_verbose()
  * @params  const char *        The file name
  * @params  const char *        The function name
  * @params  int line            The line number
- * @params  char *              The format string 
+ * @params  char *              The format string
  * @params  ...                 Variadic arguments
  *
  * @returns int                 Number of characters logged. -1 on error
  */
-int log_error(const char *file, const char * function, int line, char *format, ...) 
+int log_error(const char *file, const char * function, int line, char *format, ...)
 {
     char buffer[BUFFER_SIZE] = {0};
     uint32_t log_length = 0;
@@ -52,5 +56,36 @@ int log_error(const char *file, const char * function, int line, char *format, .
         return ret;
     }
     syslog(LOG_ERR,"%s\n", buffer);
+    return ret;
+}
+
+//! Function to log informational stuff
+/*
+ * @params  const char *        The file name
+ * @params  const char *        The function name
+ * @params  int line            The line number
+ * @params  char *              The format string
+ * @params  ...                 Variadic arguments
+ *
+ * @returns int                 Number of characters logged. -1 on error
+ */
+int log_info(const char *file, const char * function, int line, char *format, ...)
+{
+    char buffer[BUFFER_SIZE] = {0};
+    uint32_t log_length = 0;
+    va_list args ={0};
+    int ret = 0;
+
+    snprintf(buffer, BUFFER_SIZE, "%s: In function (%s), line (%d): ", file, function, line);
+    log_length = strlen(buffer);
+    va_start(args, format);
+    vsnprintf(buffer + log_length, BUFFER_SIZE - log_length, format, args);
+    va_end(args);
+
+    if (verbose) {
+        printf("%s\n", buffer);
+        return ret;
+    }
+    syslog(LOG_INFO,"%s\n", buffer);
     return ret;
 }
--- logging.h
+++ logging.h
@@ -1,4 +1,5 @@
-#define LOG_ERROR(format, ...) log_error(__FILE__, __func__, __LINE__, format, ## __VA_ARGS__) 
+#define LOG_ERROR(format, ...) log_error(__FILE__, __func__, __LINE__, format, ## __VA_ARGS__)
+#define LOG_LINE(format, ...) log_info(__FILE__, __func__, __LINE__, format, ## __VA_ARGS__)
 
 //! Function to initiate logger
 void init_log();
@@ -7,13 +8,8 @@ void init_log();
 void set_verbose();
 
 //! Function to log errors
-/*
- * @params  const char *        The file name
- * @params  const char *        The function name
- * @params  int line            The line number
- * @params  char *              The format string 
- * @params  ...                 Variadic arguments
- *
- * @returns int                 Number of characters logged. -1 on error
- */
 int log_error(const char *, const char *, int, char *, ...);
+
+//! Function to log informational stuff
+int log_info(const char *, const char *, int, char *, ...);
+
